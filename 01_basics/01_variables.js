ffb80d04dd7fce7d0f47d6e30199aea0c17b7b2f
tanishkalbus <138288874+tanishkalbus@users.noreply.github.com>
2024-03-17 09:28:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,36 @@
+const accountid = 12323
+let accountemail = "tanishkqjain2334@gmail.com"
+var accountcity = "jaipur"
+accountstate = "rajasthan"
+let accountsource;
+
+
+/*
+some notes regarding variables 
+variables are storage containter in which something get stored as memory inn our computer 
+there are three types of variables 
+
+1. var- there are not block scoped meaininng there are defined are globally they exist within the function they are declared in, or globally if declared outside any function. However, var variables are not block-scoped, meaning they can be accessed outside of conditional blocks.
+One significant characteristic of var variables is that they can be re-declared and updated within their scope.
+
+2. let - they are blocked scoped meaning they are only defined in their blocks in their let allows block-scoping of variables, meaning they exist only within the block they are defined in (e.g., loops, conditional statements, and functions). Unlike var, variables declared with let cannot be re-declared within the same block scope, but they can be updated.
+
+ 3. const - const is used to declare variables whose values cannot be re-assigned once initialized. It also provides block-scoping like 'let'.it's important to note that const does not make the variable immutable; it just prevents reassignment of the variable itself. If the variable is an object or an array, its properties or elements can still be changed.
+
+
+
+*/
+
+console.log();
+
+
+console.table([]);// use for multiple prinnt statement sepearte with comma 
+
+
+
+// some thing more about variables or in brief what are every variables 
+/*
+In JavaScript, you can declare a variable using the let keyword. This creates a block-scoped variable, meaning it's only accessible within the code block where it's declared. You can update the value of a let variable throughout your code, but you cannot re-declare it with the same name within the same block.*/
+
+
+
